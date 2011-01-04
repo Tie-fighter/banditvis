f281aa1375e7ebbad7ed55152a8caa648361b470
Thomas Steinbrenner <thomas@thomas-steinbrenner.net>
2011-01-04 23:32:23 +0100

    we need this too...

--- add_bandit.py
+++ add_bandit.py
@@ -1,47 +1,17 @@
 #!/usr/bin/env python
 
-import ConfigParser		# for config parsing
 import sys				# for argument passing
 import re				# for ip_address checking
 import MySQLdb			# for DB interaction
 import time				# for getting the time
 
+from functions import *		# import our own functions
+
 def print_usage():
 	print 'USAGE: add_bandit.py ip_address [OFFENCE]'
 	print 'additional configuration: config.ini'
 
-
-# check config
-config = ConfigParser.ConfigParser()
-config.read('config.ini')
-
-if config.get('mySQL', 'host') == '':
-	print 'ERROR: mySQL host not set'
-	print_usage()
-	exit()
-else:
-	db_host = config.get('mySQL', 'host')
-
-if config.get('mySQL', 'user') == '':
-	print 'ERROR: mySQL user not set'
-	print_usage()
-	exit()
-else:
-	db_user = config.get('mySQL', 'user')
-
-if config.get('mySQL', 'password') == '':
-	print 'ERROR: mySQL password not set'
-	print_usage()
-	exit()
-else:
-	db_password = config.get('mySQL', 'password')
-
-if config.get('mySQL', 'database') == '':
-	print 'ERROR: mySQL database not set'
-	print_usage()
-	exit()
-else:
-	db_db = config.get('mySQL', 'database')
+read_config()
 
 # check arguments
 if (len(sys.argv) < 2 or len(sys.argv) > 4):
