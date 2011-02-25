05d98d05a8476fa47e8b6c135dbcca1de956b950
Thomas Steinbrenner <thomas@thomas-steinbrenner.net>
2011-02-25 01:39:51 +0100

    switched to configuration via xml; changed color scheme, tested ShieldSymbolizers

--- draw_map.py
+++ draw_map.py
@@ -11,41 +11,20 @@ from my_functions import *		# import our own functions
 
 ##### Draw Map
 ### Instantiate a map object with given width, height and spatial reference system
-m = mapnik.Map(2400, 1158, "+proj=latlong +datum=WGS84")
-
-# Set background colour to 'steelblue'.  
-# You can use 'named' colours, #rrggbb, #rgb or rgb(r%,g%,b%) format
-m.background = mapnik.Color('steelblue')
-
-### Map Style
-style0 = mapnik.Style()
-rule0 = mapnik.Rule()
-rule0.symbols.append(mapnik.PolygonSymbolizer(mapnik.Color('#f2eff9')))
-rule0.symbols.append(mapnik.LineSymbolizer(mapnik.Color('rgb(50%,50%,50%)'),0.1))
-#rule0.symbols.append(mapnik.ShieldSymbolizer('NAME','DejaVu Sans Bold', 6, mapnik.Color('#000000'),'', '', 0, 0))
-style0.rules.append(rule0)
-
-### Data Style
-style1 = mapnik.Style()
-rule1 = mapnik.Rule()
-points = mapnik.PointSymbolizer('images/attacker.png', 'png', 5, 5)
-points.allow_overlap = True
-rule1.symbols.append(points)
-#rule1.symbols.append(mapnik.ShieldSymbolizer('ip_address','DejaVu Sans Bold', 10, mapnik.Color('#000000'),'images/attacker.png', 'png', 5, 5))
-style1.rules.append(rule1)
-
-### Add styles
-m.append_style('Map Style', style0)
-m.append_style('Data Style', style1)
+mapfile = "templates/test.xml"
+m = mapnik.Map(2400, 1158)
+mapnik.load_map(m, mapfile)
 
 ### World Layer
 layer0 = mapnik.Layer('world',"+proj=latlong +datum=WGS84")
 layer0.datasource = mapnik.Shapefile(file='data/shapes/TM_WORLD_BORDERS-0.3')
-layer0.styles.append('Map Style')
 
 ### Data Layer
 layer1 = mapnik.Layer('attackers',"+proj=latlong +datum=WGS84")
 layer1.datasource = mapnik.PostGIS(host='localhost', user='banditvis', password='UwpQUMS5cD3YdrLr', dbname='banditvis', table='bandits')
+
+### Add Styles
+layer0.styles.append('Map Style')
 layer1.styles.append('Data Style')
 
 ### Add Layers
@@ -79,9 +58,9 @@ im = Image.open('output/world.png')
 draw = ImageDraw.Draw(im)
 font = ImageFont.truetype('fonts/LinLibertine_Bd-4.1.5.otf', 20)
 ### Time of Render
-draw.rectangle((5,5, 300,50), outline = (0,0,0))
-draw.text((10, 5), time.strftime("Rendered: %d.%m.%Y %H:%M:%S GMT", time.gmtime()), (0,0,0), font=font)
-draw.text((10, 25), str(count_attacks) + " attacks / "+ str(count_ips) +" IPs", (0,0,0), font=font)
+draw.rectangle((5,5, 300,50), outline = (255,255,255))
+draw.text((10, 5), time.strftime("Rendered: %d.%m.%Y %H:%M:%S GMT", time.gmtime()), (255,255,255), font=font)
+draw.text((10, 25), str(count_attacks) + " attacks / "+ str(count_ips) +" IPs", (255,255,255), font=font)
 
 ###
 #draw.rectangle((20,250, 350,1000), outline = (0,0,0))
